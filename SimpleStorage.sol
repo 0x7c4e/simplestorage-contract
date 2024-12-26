c1d166e570afb26e86f76d6f02816fa87b2054e5
0x7c4e <iamgreatdiro@gmail.com>
2024-12-26 15:52:23 +0100

    rand

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,57 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24; //declare solidity version
+pragma solidity 0.8.26;
 
-// initialize a contract, think of contract as classes
 contract SimpleStorage {
-    // if no value is passed to a variable at initialization, it returns the default value of it's data type.
+    uint myFavoriteNumber;
 
-    uint256 myFavoriteNumber;
-
-    // ARRAY syntax
-    // uint256[] listOffavoriteNumbers;
-
-    // you can create your own types in solidity using the struct keyword
-    // we create a new type of person in which case each person of this type will have a name and a fav number
     struct Person {
-        uint256 favoriteNumber;
+        uint favoriteNumber;
         string name;
     }
 
-    // DYNAMIC AND STATIC ARRAYS
-    // dynamic arrays can be expanded and take in more values [], static arrays can only take in as much as isspecified in the declaration. [3]
     Person[] public listOfPeople;
 
-    // MAPPING
-    // mappings are key-value pairs for storing data
-    // here we create a mapping where the key is a string that points to a uint256, followed by the visibility of the map and its name
-    // the default values of a mapping's key is 0
-    mapping(string => uint256) public nameToFavoriteNumber;
+    mapping (string => uint) public nameToFavoriteNumber;
 
-
-    // Person public sage = Person({
-    //     favoriteNumber: 7,
-    //     name: "Sage"
-    // });
-
-    // FUNCTIONS
-
-    // the virtual  keyword indicates that this fucntion is overridable
-    function store(uint256 _favoriteNumber) public virtual  {
+    function store(uint _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
 
-    // GETTER FUNCTION
-    // using the 'view' keyword here indicates that this function isn't meant to run or execute a transaction (any action that updates the state of the blockchain).
-    // it simply 'views' or 'reads' data from the blockchain
-    // the returns keyword specifies what type of value the function will give when called.
-    function retrieve() public  view  returns(uint256) {
-        return  myFavoriteNumber;
+    function retrieve() public view returns(uint) {
+        return myFavoriteNumber;
     }
 
-    // function to add people to the list
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push(Person( _favoriteNumber, _name));
-        // add a person to te map
+    function addPerson(string memory _name, uint _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 
